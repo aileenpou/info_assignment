624ff3b58a6e915d6b26ee7e25fdb03a37839670
aileenpou <64500383+aileenpou@users.noreply.github.com>
2020-07-07 18:54:41 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 1
+# Type the answer in the checkbox, and then the reply will show
+
+# 2
+# Type it again in the checkbox, the reply will change again.
---
+++ public/index.js
@@ -0,0 +1,27 @@
+// Librairies
+import Framework7 from 'framework7/framework7.esm.bundle'
+
+// Init F7
+new Framework7({
+  root: '#app',
+  theme: 'auto',
+  // Fix for iPhone X notch
+  statusbar: {
+    overlay: Framework7.device.ios ? Framework7.device.webView || Framework7.device.cordova : 'auto',
+  },
+})
+
+document.getElementById("magic").addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const aAnswers = ["no", "yes"];
+  const nAnswer = Math.floor(Math.random() * aAnswers.length);
+  document.getElementById("answer").innerHTML = aAnswers[nAnswer];
+});
+
+if (`serviceWorker` in navigator) {
+  window.addEventListener(`load`, () => {
+    // const sServiceWorker = `/sw.js`;
+    //    navigator.serviceWorker.register(sServiceWorker)
+    navigator.serviceWorker.register(`/sw.js`)
+  })
+}
